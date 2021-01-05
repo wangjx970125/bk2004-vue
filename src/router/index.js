59b40b197c769566902ccaa20b3dd75da4944687
wangjx970125 <a805435642@163.com>
2021-01-05 14:05:00 +0800

    1月04日改

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,61 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+// 导入需要的组件
+
+// 导入经过模块化的路由
+import filmRouter from './routes/film'
+import cinemaRouter from './routes/cinema'
+import centerRouter from './routes/center'
+
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path:"/",
+    redirect:"/films/nowPlaying"
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // 模块化
+  //需要展开, 导出的是一个数组,但是人家想要的是一个对象
+  ...filmRouter,  
+  ...cinemaRouter,
+  ...centerRouter,
+
+  // 电影模块
+  //    电影模块父      /films
+  //    正在热映        /films/nowPlaying
+  //    即将上映        /filme/comingSoon
+  //   电影详情         /film/:filmId
+
+  // {
+  //   path:"/films",
+  //   component:Films,
+  //   children:[
+  //     {path:"nowPlaying",component: NowPlaying },
+  //     {path:"comingSoon",component: ComingSoon },
+  //   ],
+  // },
+  // {
+  //   path:"/film/:filmId",
+  //   component:Detail
+  // },
+
+
+
+  //影院模块
+  //  /cinemas
+  // {
+  //   path:"/cinemas",
+  //   component:Cinemas,
+  // },
+
+  //我的模块
+  //  /center
+  // {
+  //   path:"/center",
+  //   component:Center,
+  // },
+  
 ]
 
 const router = new VueRouter({
---
+++ src/router/routes/cinema.js
@@ -0,0 +1,7 @@
+import Cinemas from "@/views/Cinemas/Index"
+
+var cinema = [{
+    path:"/cinemas",
+    component:Cinemas,
+  },]
+export default cinema;
